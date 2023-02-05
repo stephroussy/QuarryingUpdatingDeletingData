465d5b52be3f931e889ac3033c1e640e90cdfce4
stephroussy <stephroussy@hotmail.com>
2023-02-04 22:16:27 -0700

    week 11 assignment

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -8,6 +8,7 @@ import java.util.NoSuchElementException;
 
 import projects.dao.ProjectDao; 
 import projects.entity.Project;
+import projects.exception.DbException;
 
 
 public class ProjectService {
@@ -24,6 +25,17 @@ public class ProjectService {
 	
 	public Project fetchProjectById(Integer projectId) {
 		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist."));
+	}
+	
+	public void modifyProjectDetails (Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+	}
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Proiect with ID=" + projectId + " does not exist.");
+		}
 	}
 }
 
